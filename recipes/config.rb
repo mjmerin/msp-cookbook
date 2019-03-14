357b1842c45f8d93786b82fcf69858afa7b352cf
Mark John Merin <mjmerin@gmail.com>
2019-03-14 12:53:42 -0700

    download dotfiles from my github

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,1 +1,11 @@
+user = node['msp']['admin_user']
+user_dir = mac_os_x? ? '/Users' : '/home'
+user_home = ::File.join(user_dir, user)
+dotfile = mac_os_x? ? '.bash_profile' : '.profile'
+
 hostname node['msp']['hostname']
+
+remote_file ::File.join(user_home, dotfile) do
+  source 'https://raw.githubusercontent.com/mjmerin/config_files/master/.bash_profile'
+  action :create
+end
