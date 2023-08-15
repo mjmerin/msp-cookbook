4f11887c8b1fa236133c6ed09bf80bb790153e9e
Mark John Merin <mjmerin@gmail.com>
2023-08-15 14:26:22 -0700

    add guards for git config

--- recipes/git.rb
+++ recipes/git.rb
@@ -13,16 +13,20 @@ user_email = node['msp']['admin_user'] + '@gmail.com'
 
 execute "Set git user name to #{user_name}" do
   command "git config --global user.name \"#{user_name}\""
+  only_if "git -v | grep version"
 end
 
 execute "Set git user email to #{user_email}" do
   command "git config --global user.email #{user_email}"
+  only_if "git -v | grep version"
 end
 
 execute 'Set git core editor to emacs' do
   command 'git config --global core.editor emacs'
+  only_if "git -v | grep version"
 end
 
-execute "Copy .gitconfig over to #{user_home}" do
+execute "Copy .gitconfig from vagrant user home over to #{user_home}" do
   command "cp /Users/vagrant/.gitconfig #{gitconfig}"
+  only_if "git -v | grep version"
 end
