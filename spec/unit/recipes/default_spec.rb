d0ef88f4d1db1fe6f2a8480ac0e9862e5e40a663
Mark John Merin <mjmerin@gmail.com>
2023-08-16 14:50:31 -0700

    fix broken chef spec test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,30 +2,11 @@
 # Cookbook:: msp
 # Spec:: default
 #
-# Copyright:: 2019, Mark Merin, All Rights Reserved.
+# Copyright:: 2023, Mark Merin, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'msp::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on Ubuntu 18.04' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+  context 'When all attributes are default, on Ubuntu 23.04' do
   end
 end
