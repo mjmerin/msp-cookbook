7ef025d53c2c348a0c0aaaed21e113db0c57861a
Mark John Merin <mjmerin@gmail.com>
2023-08-16 15:49:25 -0700

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'msp'
 maintainer 'Mark Merin'
 license 'All Rights Reserved'
 description 'Installs/Configures Merin systems.'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 14.0'
 
 issues_url 'https://github.com/mjmerin/msp-cookbook/issues'
