8f41d75c29321c982e4b4c36318bb04d63beb2ce
Mark John Merin <mjmerin@gmail.com>
2023-08-16 13:35:53 -0700

    cleanup directory paths

--- test/integration/default/controls/directories_test.rb
+++ test/integration/default/controls/directories_test.rb
@@ -1,15 +1,15 @@
-title 'directories'
-coding = '/Users/mjmerin/coding'
-docs = '/Users/mjmerin/Documents'
-dirs = [coding, docs]
+
+directories = %w(coding, docs)
 
 control 'directories' do
   title 'what directories are present'
-  desc 'Verify directories are present'
+  desc 'Verify specified directories are present'
 
   if os.darwin?
-    dirs.each do |dir|
-      describe directory(dir) do
+    directories.each do |dir|
+
+      directory_path = ::File.join('/Users', 'mjmerin', dir)
+      describe directory(directory_path) do
         it { should exist }
       end
     end
