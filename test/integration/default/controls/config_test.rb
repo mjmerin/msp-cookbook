1f22857db50473bd37a7002a80128ff17df081f0
Mark John Merin <mjmerin@gmail.com>
2023-08-16 13:34:43 -0700

    cookstyle changes

--- test/integration/default/controls/config_test.rb
+++ test/integration/default/controls/config_test.rb
@@ -1,26 +1,23 @@
-user_home = '/Users/mjmerin/'
-
 control 'dotfiles' do
   title 'what dot files are available'
   desc 'Verify dot files existence'
 
   if os.darwin?
-      describe directory('/Users/mjmerin/.zshrc') do
-        it { should exist }
-      end
+    describe directory('/Users/mjmerin/.zshrc') do
+      it { should exist }
+    end
 
-      describe command('echo $SHELL') do
-        its(stdout) {should match '/bin/zsh' }
-      end
+    describe command('echo $SHELL') do
+      its(stdout) { should match '/bin/zsh' }
+    end
   else
     describe directory('/Users/home/mjmerin/.profile') do
       it { should exist }
     end
 
     describe command('echo $SHELL') do
-      its(stdout) {should match '/bin/bash' }
+      its(stdout) { should match '/bin/bash' }
     end
 
   end
-
 end
