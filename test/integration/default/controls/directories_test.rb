0f457c7700e1a0f76fbd08193cc9513de4a504e4
Mark John Merin <mjmerin@gmail.com>
2023-08-16 15:39:41 -0700

    remove OS check from wrapping the inspec resource and have it wrap the path variable instead

--- test/integration/default/controls/directories_test.rb
+++ test/integration/default/controls/directories_test.rb
@@ -5,12 +5,16 @@ control 'directories' do
   title 'what directories are present'
   desc 'Verify specified directories are present'
 
-  if os.darwin?
-    directories.each do |dir|
-      directory_path = ::File.join('/Users', 'mjmerin', dir)
-      describe directory(directory_path) do
-        it { should exist }
-      end
+  user_home = if os.darwin?
+                '/Users/mjmerin'
+              else
+                '/home/mjmerin/'
+              end
+
+  directories.each do |dir|
+    directory_path = ::File.join(user_home, dir)
+    describe directory(directory_path) do
+      it { should exist }
     end
   end
 end
