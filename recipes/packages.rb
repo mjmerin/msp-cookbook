7c2e836bed4cbcb6cd45b90fd9534bf06381acbd
Mark John Merin <mjmerin@gmail.com>
2023-08-17 12:35:07 -0700

    just use the chromium browser for ubuntu

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -7,22 +7,6 @@
 if linux?
   packages = node['msp']['std']['base_pkgs'] + node['msp']['std']['dev_pkgs']
 
-  apt_repository 'chrome' do
-    uri 'http://dl.google.com/linux/chrome/deb'
-    distribution 'stable'
-    components %w(main)
-    key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
-  end
-
-  apt_repository 'vscode' do
-    uri 'https://packages.microsoft.com/repos/vscode'
-    distribution 'stable'
-    components %w(main)
-    key 'https://packages.microsoft.com/keys/microsoft.asc'
-  end
-
-  apt_update
-
   packages = packages + node['msp']['nix']['base_pkgs'] + node['msp']['nix']['dev_pkgs']
 
   packages.each do |install|
